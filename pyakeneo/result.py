9a106407eafa601d65f7a50bc85489c0b45c8297
Amat Martinez Vila <amatmv@gmail.com>
2023-03-09 12:10:10 +0100

    feat: enable result class to handle non paginated

--- pyakeneo/result.py
+++ pyakeneo/result.py
@@ -1,25 +1,39 @@
-# -*- coding: utf-8 -*-
+from __future__ import annotations
 
 import json
+from typing import Dict, Iterable
+
+import requests
 
 
 class Result(object):
-    """Holds the result of a search. It can be iterated through as a list,
+    """
+    Holds the result of a search. It can be iterated through as a list,
     as an iterator, or as a generator. Note that only one iteration should be
     used on a given Collection object.
     Search results are paginated: https://api.akeneo.com/documentation/pagination.html
 
-    The next page will be loaded once the user
-    iterated over the whole current page. The content of the new page will replace
-    the content of the previous page."""
+    The next page will be loaded once the user iterated over the whole current page.
+    The content of the new page will replace the content of the previous page.
+    """
 
-    def __init__(self, session, count, current_items, link_first, link_next, link_self):
-        self._items = current_items
+    def __init__(
+        self,
+        session: requests.Session,
+        *,
+        items: list | dict,
+        count: int,
+        link_first: str,
+        link_next: str,
+        link_self: str,
+    ):
+        self._session = session
+        self._items = items
+        self._count = count
         self._link_next = link_next
         self._link_self = link_self
         self._link_first = link_first
-        self._count = count
-        self._session = session
+
         self._page_iterator = iter(self._items)
         self._reached_the_end = False
 
@@ -32,7 +46,7 @@ class Result(object):
     def __next__(self):
         try:
             return next(self._page_iterator)
-        except Exception:
+        except StopIteration:
             self.fetch_next_page()
             if not self._reached_the_end:
                 return next(self._page_iterator)
@@ -44,16 +58,16 @@ class Result(object):
 
     def fetch_next_page(self):
         """Return True if a next page exists. Returns False otherwise."""
-        if self._link_next:
+        if self.is_paginated(self._items) and self._link_next:
             r = self._session.get(self._link_next)
-            if r.status_code == 200:
-                (
-                    self._link_first,
-                    self._link_self,
-                    self._link_next,
-                    self._items,
-                    self._count,
-                ) = Result.parse_page(json.loads(r.text))
+            if r.status_code == requests.codes.ok:
+                next_page = Result.parse_page(json.loads(r.text))
+                self._items = next_page["items"]
+                self._count = next_page["count"]
+                self._link_next = next_page["link_next"]
+                self._link_self = next_page["link_self"]
+                self._link_first = next_page["link_first"]
+
                 self._page_iterator = iter(self._items)
                 self._reached_the_end = False
             else:
@@ -74,23 +88,46 @@ class Result(object):
     def get_first_link(self):
         return self._link_first
 
-    @staticmethod
-    def parse_page(json_data):
+    @classmethod
+    def parse_page(cls, json_data: dict) -> Dict:
         """Returns (next link, retrieved items, count of items)"""
         final_next_link = None
         next_link = json_data["_links"].get("next")
         if next_link:
             final_next_link = next_link["href"]
-        return (
-            json_data["_links"]["first"]["href"],
-            json_data["_links"]["self"]["href"],
-            final_next_link,
-            json_data["_embedded"]["items"],
-            json_data.get("items_count"),
-        )
+        return {
+            "items": json_data["_embedded"]["items"],
+            "count": json_data.get("items_count"),
+            "link_first": json_data["_links"]["first"]["href"],
+            "link_next": final_next_link,
+            "link_self": json_data["_links"]["self"]["href"],
+        }
+
+    @classmethod
+    def parse_result(cls, session: requests.Session, json_data: dict | list):
+        if cls.is_paginated(json_data):
+            return cls(session, **cls.parse_page(json_data))
+        else:
+            return cls(session, **cls.parse_non_paginated(json_data))
 
     @staticmethod
-    def from_json_text(session, json_text):
+    def from_json_text(session: requests.Session, json_text: str) -> "Result":
         json_data = json.loads(json_text)
-        parsed = Result.parse_page(json_data)
-        return Result(session, parsed[4], parsed[3], parsed[0], parsed[2], parsed[1])
+        return Result.parse_result(session, json_data)
+
+    @classmethod
+    def is_paginated(cls, json_data: dict | list):
+        return isinstance(json_data, dict) and "_links" in json_data
+
+    @classmethod
+    def parse_non_paginated(cls, json_data: list) -> Dict:
+        link_self = ""
+        if json_data and "_links" in json_data[0]:
+            link_self = json_data["_links"]["self"]["href"]
+        return {
+            "items": json_data,
+            "count": len(json_data),
+            "link_first": "",
+            "link_next": "",
+            "link_self": link_self,
+        }
